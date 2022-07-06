0fa095dc53eff5483edb9d11f638aa0820f43529
Gabriel Manzini <manzini.gabriel@hotmail.com>
2022-07-06 20:07:50 -0300

    Revert "clear files"
    This reverts commit c04b96a3f6169829a08ace0ab3584933ffed40b4.

---
+++ HTTPUtils/TokenGeneration.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TokenUtils
+{
+    public static class TokenGeneration
+    {
+        public static long GenerateToken(string token, int numberofRotations)
+        {
+            try
+            {
+                var array = token.Select(x => x - 48).ToArray();
+
+                int auxVariable;
+                for (int i = 0; i < numberofRotations; i++)
+                {
+                    for (int j = 0; j < array.Length - 1; j++)
+                    {
+                        auxVariable = array[0];
+                        array[0] = array[j + 1];
+                        array[j + 1] = (byte)auxVariable;
+                    }
+                }
+                return Convert.ToInt64(String.Join("", array));
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception("An error occurred during the token generation process: " + ex.Message);
+            }
+           
+        }
+    }
+}
